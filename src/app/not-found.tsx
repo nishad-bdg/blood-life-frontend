1607e0a463b526824f6afb2b1ba8948435590cff
nishad-islam <nishad.nik@gmail.com>
2025-11-08 11:29:29 +0600

    feat: add NotFound component for handling 404 errors

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main style={{ padding: 32 }}>
+      <h1>Page not found</h1>
+      <p>Sorry, we couldn’t find what you’re looking for.</p>
+    </main>
+  )
+}
